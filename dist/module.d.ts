c984b5714af83bc5f927f9cd26b51326161955ae
Cezar Augusto <boss@cezaraugusto.net>
2024-03-07 16:37:10 -0300

    Rewrite to TS

--- dist/module.d.ts
+++ dist/module.d.ts
@@ -1,7 +1,22 @@
 declare function presetExtensionCreate(opts: any): {
-    presets: string[];
-    plugins: never[];
-    overrides: {};
+    presets: (string | {
+        development: boolean;
+    })[][];
+    plugins: (string | (string | {
+        absoluteRuntime: boolean;
+        corejs: boolean;
+        helpers: boolean;
+        regenerator: boolean;
+    })[])[];
+    overrides: {
+        test?: undefined;
+        plugins?: undefined;
+    } | {
+        test: RegExp;
+        plugins: (string | {
+            legacy: boolean;
+        })[][];
+    };
 };
 
 export { presetExtensionCreate as default };
